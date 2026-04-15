15e56d263624d38976575b44c4bd151220c7b120
Fizza-77 <Fizzaijaz2@gmail.com>
2026-04-15 19:17:52 +0500

    LCP fix v4.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { unoptimized: true },
   reactCompiler: true,
   experimental: {
     optimizePackageImports: ["@/components", "@/sections", "@/lib"],
